8f14cd00672b7ba2918fd28043094c725cfc4f2b
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-07-24 12:21:38 -0700

    add media queries for navbar and contact form - base media queries for mobile complete

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,50 +6,50 @@ export default function Contact() {
     <div className="contact-container">
       <form className="contact-form">
         <div className="contact-services-container">
-        <div className="contact-info">
-          <label>Name:
-            <input type="text" required/>
-          </label>
-          <label>Email:
-            <input type="email" required/>
-          </label>
-          <label>Phone:
-            <input type="text" />
-          </label>
-          <label>Property Type:        
-            <select form="contact-form">
-              <option value="Strata">Strata</option>
-              <option value="Commercial">Commercial</option>
-              <option value="Residential">Residential</option>
-            </select>
-          </label>
-        </div>
-        <div className="services">
-          <div className="service-list">          
-            <div>
-              <label for="lawn-care">Lawn Care</label>
-              <input type="checkbox" name="lawn-care" value="Lawn Care"/>
-            </div>
-            <div>
-              <label for="garden-care">Garden Care</label>
-              <input type="checkbox" name="garden-care" value="garden-care"/>
-            </div>
-            <div>
-              <label for="pruning-service">Pruning Service</label>
-              <input type="checkbox" name="pruning-service" value="pruning-service"/>
-            </div>
-            <div>      
-              <label for="pressure-washing">Pressure Washing</label>
-              <input type="checkbox" name="pressure-washing" value="pressure-washing"/>
-            </div>
-            <div> 
-              <label for="irrigation">Irrigation</label>
-              <input type="checkbox" name="irrigation" value="irrigation"/>
-            </div>
-            <div>
-              <label for="other">Other</label>
-              <input type="checkbox" name="other" value="other"/>
-            </div>
+          <div className="contact-info">
+            <label>Name:
+              <input type="text" required/>
+            </label>
+            <label>Email:
+              <input type="email" required/>
+            </label>
+            <label>Phone:
+              <input type="text" />
+            </label>
+            <label>Property Type:        
+              <select form="contact-form">
+                <option value="Strata">Strata</option>
+                <option value="Commercial">Commercial</option>
+                <option value="Residential">Residential</option>
+              </select>
+            </label>
+          </div>
+          <div className="services">
+            <div className="service-list">          
+              <div>
+                <label for="lawn-care">Lawn Care</label>
+                <input type="checkbox" name="lawn-care" value="Lawn Care"/>
+              </div>
+              <div>
+                <label for="garden-care">Garden Care</label>
+                <input type="checkbox" name="garden-care" value="garden-care"/>
+              </div>
+              <div>
+                <label for="pruning-service">Pruning Service</label>
+                <input type="checkbox" name="pruning-service" value="pruning-service"/>
+              </div>
+              <div>      
+                <label for="pressure-washing">Pressure Washing</label>
+                <input type="checkbox" name="pressure-washing" value="pressure-washing"/>
+              </div>
+              <div> 
+                <label for="irrigation">Irrigation</label>
+                <input type="checkbox" name="irrigation" value="irrigation"/>
+              </div>
+              <div>
+                <label for="other">Other</label>
+                <input type="checkbox" name="other" value="other"/>
+              </div>
             </div>
           </div>
         </div>
