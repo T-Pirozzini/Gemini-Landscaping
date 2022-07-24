2e68f2bd26ae6edff7f51fdd54b02bd661d2567d
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-07-24 13:21:02 -0700

    add emailjs and submit form info to email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,54 +1,74 @@
 import React from 'react'
 import "./Contact.css"
+import emailjs from 'emailjs-com'
 
 export default function Contact() {
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+    console.log(e.target)
+    emailjs.sendForm(
+      'service_k56wdek',
+      'template_vrspn8p',
+      e.target,
+      'fZzoE3AXoEhioZYVZ'
+      ).then(res => {
+        console.log(res)
+      }).catch(err => {
+        console.log(err)
+    })
+  }
+
   return (
     <div className="contact-container">
-      <form className="contact-form">
+      <form className="contact-form" onSubmit={sendEmail}>
         <div className="contact-services-container">
           <div className="contact-info">
             <label>Name:
-              <input type="text" required/>
+              <input type="text" name="name" required/>
             </label>
             <label>Email:
-              <input type="email" required/>
+              <input type="email" name="email" required/>
             </label>
             <label>Phone:
-              <input type="text" />
+              <input type="text" name="phone" />
             </label>
             <label>Property Type:        
-              <select form="contact-form">
-                <option value="Strata">Strata</option>
-                <option value="Commercial">Commercial</option>
-                <option value="Residential">Residential</option>
+              <select form="contact-form" name='property'>
+                <option value="Strata" name='property'>Strata</option>
+                <option value="Commercial" name='property'>Commercial</option>
+                <option value="Residential" name='property'>Residential</option>
               </select>
+            </label>
+            <label>Property Address:
+              <input type="text" name="address" />
             </label>
           </div>
           <div className="services">
             <div className="service-list">          
               <div>
                 <label for="lawn-care">Lawn Care</label>
-                <input type="checkbox" name="lawn-care" value="Lawn Care"/>
+                <input type="checkbox" name="services" value="Lawn Care"/>
               </div>
               <div>
                 <label for="garden-care">Garden Care</label>
-                <input type="checkbox" name="garden-care" value="garden-care"/>
+                <input type="checkbox" name="services" value="garden-care"/>
               </div>
               <div>
                 <label for="pruning-service">Pruning Service</label>
-                <input type="checkbox" name="pruning-service" value="pruning-service"/>
+                <input type="checkbox" name="services" value="pruning-service"/>
               </div>
               <div>      
                 <label for="pressure-washing">Pressure Washing</label>
-                <input type="checkbox" name="pressure-washing" value="pressure-washing"/>
+                <input type="checkbox" name="services" value="pressure-washing"/>
               </div>
               <div> 
                 <label for="irrigation">Irrigation</label>
-                <input type="checkbox" name="irrigation" value="irrigation"/>
+                <input type="checkbox" name="services" value="irrigation"/>
               </div>
               <div>
                 <label for="other">Other</label>
-                <input type="checkbox" name="other" value="other"/>
+                <input type="checkbox" name="services" value="other"/>
               </div>
             </div>
           </div>
@@ -57,7 +77,7 @@ export default function Contact() {
         
         <div className="details">        
           <p>Details:</p>
-          <textarea></textarea>
+          <textarea name="details"></textarea>
           <div className="contact-btn-container">
             <button className="btn-contact">SUBMIT</button>
           </div>
