db9e5e84f8c13a090b28b55d5e3cb4d051537998
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-07-25 12:15:41 -0700

    minor style and media query adjustments before deployment

--- src/components/ServiceCarousel.jsx
+++ src/components/ServiceCarousel.jsx
@@ -41,28 +41,28 @@ export default function ServiceCarousel() {
       >
         <div className="lawn">
           <h2>Lawn Care</h2>
-          <button className="btn-book-now">Book Now</button>
+          <button className="btn-book-now" onClick={handleClick}>Book Now</button>
         </div>
         <div className="garden">
           <h2>Garden Care & Pruning</h2>
-          <button className="btn-book-now">Book Now</button>
+          <button className="btn-book-now" onClick={handleClick}>Book Now</button>
         </div>
         <div className="all-services">
           <h2>All Services</h2>
-          <button className="btn-book-now">Book Now</button>
+          <button className="btn-book-now" onClick={handleClick}>Book Now</button>
         </div>
         <div className="pressure-washing">
           <h2>Pressure Washing</h2>
-          <button className="btn-book-now">Book Now</button>
+          <button className="btn-book-now" onClick={handleClick}>Book Now</button>
         </div>
         <div className="other-services">
-          <h2>Other Services Available</h2>
+          <h2>Other Services</h2>
           <div className="other-services-list">
             <p>Retaining walls</p>
             <p>Irrigation</p>
             <p>Light construction</p>
-            <p>Bark mulch application</p>
-            <p>River rock themed landscapes</p>
+            <p>Bark mulch</p>
+            <p>Rock landscapes</p>
           </div>          
           <button className="btn-book-now" onClick={handleClick}>Book Now</button>
         </div>
